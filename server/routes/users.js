9816b420c7d8ed09f4ef5970118e53c15b2315a7
Igor Silveira <up201505172@fe.up.pt>
2018-11-16 00:46:42 +0000

    initial commit

---
+++ server/routes/users.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  var list = [
+    {
+      name: "list1",
+      description: "a simple example list"
+    },
+    {
+      name: "list2",
+      description: "another example list"
+    },
+    {
+      name: "list3",
+      description: "yet another"
+    },
+  ];
+  res.json(list);
+  console.log('Sent list of items');
+});
+
+module.exports = router;
