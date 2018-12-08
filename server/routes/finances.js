0716cc1b30044a83906f917895d15f4749620dcf
ltiagosantos <1140927@isep.ipp.pt>
2018-12-08 23:17:17 +0000

    Added chart to finances.

--- server/routes/finances.js
+++ server/routes/finances.js
@@ -21,4 +21,90 @@ router.get('/', function(req, res, next) {
   console.log('Sent list of items');
 });
 
+/* GET equity values. */
+router.get('/equity', function(req, res, next) {
+  
+  var equity = [
+    {
+        'year': 2013,
+        'value': 11000
+    },
+    {
+        'year': 2014,
+        'value': 13000
+    },
+    {
+        'year': 2015,
+        'value': 10000
+    },
+    {
+        'year': 2016,
+        'value': 11000
+    },
+    {
+        'year': 2017,
+        'value': 12000
+    }
+  ];
+  res.json(equity);
+});
+
+
+/* GET liability values. */
+router.get('/liability', function(req, res, next) {
+  
+  var liability = [
+    {
+        'year': 2013,
+        'value': 11000
+    },
+    {
+        'year': 2014,
+        'value': 13000
+    },
+    {
+        'year': 2015,
+        'value': 10000
+    },
+    {
+        'year': 2016,
+        'value': 11000
+    },
+    {
+        'year': 2017,
+        'value': 12000
+    }
+  ];
+  res.json(liability);
+});
+
+/* GET asset values. */
+router.get('/assets', function(req, res, next) {
+  
+  var assets = [
+    {
+        'year': 2013,
+        'value': 11000
+    },
+    {
+        'year': 2014,
+        'value': 13000
+    },
+    {
+        'year': 2015,
+        'value': 10000
+    },
+    {
+        'year': 2016,
+        'value': 11000
+    },
+    {
+        'year': 2017,
+        'value': 12000
+    }
+  ];
+  res.json(assets);
+});
+
+
 module.exports = router;
