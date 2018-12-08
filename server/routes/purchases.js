269b53e1e92e2dfb82cc283eb0e02cf76c87a466
ltiagosantos <1140927@isep.ipp.pt>
2018-12-08 22:05:51 +0000

    Request info from endpoints and displays it on chart.

--- server/routes/purchases.js
+++ server/routes/purchases.js
@@ -4,23 +4,53 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  var list = [
+  var cityData = [
     {
-      name: "list1",
-      description: "a simple example list"
+        'cityName': 'Debrecen',
+        'population': 237888,
+        'income': 135430
     },
     {
-      name: "list2",
-      description: "another example list"
-    },
-    {
-      name: "list3",
-      description: "yet another"
-    },
+        'cityName': 'Miskolc',
+        'population': 216470,
+        'income': 151102
+    }
   ];
-  res.json(list);
-  console.log('Sent list of items');
+
+  res.json(cityData);
+
 });
+
+
+/* GET purchases volume. */
+router.get('/volume', function(req, res, next) {
+  
+  var volume = [
+    {
+        'year': 1870,
+        'population': 5011310
+    },
+    {
+        'year': 1890,
+        'population': 6009351
+    },
+    {
+        'year': 1910,
+        'population': 7612114
+    },
+    {
+        'year': 1930,
+        'population': 8685109
+    },
+    {
+        'year': 1949,
+        'population': 9204799
+    }
+  ];
+  
+    res.json(volume);
+  
+  });
 
 router.get('/filter', function(req, res, next) {
 
