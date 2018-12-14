ca4ce5be05ffe6ec1f8cbaabcb1b20eed10b377a
GilTeixeira <up201505735@fe.up.pt>
2018-12-14 18:41:44 +0000

    Finish StockMovement

--- pp/populator/inserters/line.js
+++ pp/populator/inserters/line.js
@@ -11,16 +11,20 @@
 module.exports = function (ln, iID, smID) {
 
     ins.taxTableEntry(ln.Tax).then(taxID => {
+        
+        //console.log(ln);
 
-        let IECAmount, stockMovementID, invoiceID, orderRefOriginatingOn, orderRefOrderDate;
+        let IECAmount, stockMovementID, invoiceID, orderRefOriginatingOn, orderRefOrderDate, taxPointDate;
         IECAmount = (ln.CustomsInformation == undefined ? null : ln.CustomsInformation.IECAmount._text);
         stockMovementID = smID;
         invoiceID = iID;
         orderRefOriginatingOn = (ln.OrderReferences == undefined ? null : ln.OrderReferences.OriginatingON._text);
         orderRefOrderDate = (ln.OrderReferences == undefined ? null : ln.OrderReferences.OrderDate._text);
+        taxPointDate = (ln.TaxPointDate == undefined ? null : ln.TaxPointDate._text);
+
 
         let sql = "INSERT INTO line (line_number, product_code_id, quantity, unit_of_measure, unit_price, tax_point_date, description, credit_amount, tax_id, settlement_amount, iec_amount, arc_no, stock_movement_id, invoice_id, order_ref_originating_on, order_ref_order_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE id=id";
-        let inserts = [ln.LineNumber._text, ln.ProductCode._text, ln.Quantity._text, ln.UnitOfMeasure._text, ln.UnitPrice._text, ln.TaxPointDate._text, ln.Description._text, ln.CreditAmount._text, taxID, ln.SettlementAmount._text, IECAmount, null, stockMovementID, invoiceID, orderRefOriginatingOn, orderRefOrderDate];
+        let inserts = [ln.LineNumber._text, ln.ProductCode._text, ln.Quantity._text, ln.UnitOfMeasure._text, ln.UnitPrice._text, taxPointDate, ln.Description._text, ln.CreditAmount._text, taxID, ln.SettlementAmount._text, IECAmount, null, stockMovementID, invoiceID, orderRefOriginatingOn, orderRefOrderDate];
         sql = mysql.format(sql, inserts);
 
         return new Promise(function (resolve, reject) {
--- pp/populator/inserters/stockMovement.js
+++ pp/populator/inserters/stockMovement.js
@@ -7,22 +7,76 @@
  * @return {Promise}
  */
 module.exports = function (sm) {
-    
+
     // let GroupingCode, TaxonomyCode;
     // GroupingCode = (acc.GroupingCode == undefined ? null : acc.GroupingCode._text);
     // let TransactionID = (i.TransactionID == undefined ? null : i.TransactionID._text);
 
-    let sql = "INSERT INTO stock_movement (document_number, atcud, ds_movement_status, ds_movement_status_date, ds_source_id, ds_source_billing, hash, hash_control, period, movement_date, movement_type, system_entry_date, customer_id, source_id, ship_to_id, ship_from_id, movement_start_time, movement_of_goods_id, document_totals_tax_payable, document_totals_net_total, document_totals_gross_total) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE document_number=document_number";
-    let inserts = [sm.DocumentNumber._text, sm.ATCUD._text, sm.DocumentStatus.MovementStatus._text, sm.DocumentStatus.MovementStatusDate._text, sm.DocumentStatus.SourceID._text, sm.DocumentStatus.SourceBilling._text, sm.Hash._text, sm.HashControl._text, sm.Period._text, sm.MovementDate._text, sm.MovementType._text, sm.SystemEntryDate._text, null, sm.SourceID._text, null, null, sm.MovementStartTime._text, MovementGoodsID, sm.DocumentTotals.TaxPayable._text, sm.DocumentTotals.NetTotal._text, sm.DocumentTotals.GrossTotal._text];
-    sql = mysql.format(sql, inserts);
-    
-    return new Promise(function (resolve, reject) {
-        con.query(sql, function (err, result) {
-            if (err){
-                console.log(sql);
-                throw err;
-            }
-            resolve(true);
+    addAddresses(sm).then(addrIDs => {
+        let stID = addrIDs[0];
+        let sfID = addrIDs[1];
+
+        let sql = "INSERT INTO stock_movement (document_number, atcud, ds_movement_status, ds_movement_status_date, ds_source_id, ds_source_billing, hash, hash_control, period, movement_date, movement_type, system_entry_date, customer_id, source_id, ship_to_id, ship_from_id, movement_start_time, movement_of_goods_id, document_totals_tax_payable, document_totals_net_total, document_totals_gross_total) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE document_number=document_number";
+        let inserts = [sm.DocumentNumber._text, sm.ATCUD._text, sm.DocumentStatus.MovementStatus._text, sm.DocumentStatus.MovementStatusDate._text, sm.DocumentStatus.SourceID._text, sm.DocumentStatus.SourceBilling._text, sm.Hash._text, sm.HashControl._text, sm.Period._text, sm.MovementDate._text, sm.MovementType._text, sm.SystemEntryDate._text, sm.CustomerID._text, sm.SourceID._text, stID, sfID, sm.MovementStartTime._text, MovementGoodsID, sm.DocumentTotals.TaxPayable._text, sm.DocumentTotals.NetTotal._text, sm.DocumentTotals.GrossTotal._text];
+        sql = mysql.format(sql, inserts);
+
+        return new Promise(function (resolve, reject) {
+            con.query(sql, function (err, result) {
+                if (err) {
+                    console.log(sql);
+                    throw err;
+                }
+                // insert each line
+                if (typeof sm.Line !== 'undefined') {
+                    if (Array.isArray(sm.Line)) {
+                        sm.Line.forEach(line => {
+                            ins.line(line, null, sm.DocumentNumber._text);
+                        });
+                    } else {
+                        ins.line(sm.Line, null, sm.DocumentNumber._text);
+                    }
+                }
+                resolve(true);
+            });
+        })
+    });
+};
+
+
+/**
+ * 
+ * @param {*} sm Stock Movement
+ * @return {Promise} with the ids of both the ship to and the ship from, by this order.
+ */
+function addAddresses(sm) {
+    return new Promise(function (resolve, error) {
+        let pst = ins.address(sm.ShipTo.Address);
+        let psf = ins.address(sm.ShipFrom.Address);
+
+        Promise.all([pst, psf]).then(addrIDs => {
+            let stAID = addrIDs[0];
+            let sfAID = addrIDs[1];
+            resolve(addShips(sm, stAID, sfAID));
         });
     })
-};
+}
+
+/**
+ * 
+ * @param {*} sm Stock Movement
+ * @param {*} stAID ship to adress ID 
+ * @param {*} sfAID ship from adress ID
+ * @return {Promise} with the ids of both the ship to address and the ship from address, by this order.
+ */
+function addShips(sm, stAID, sfAID) {
+    return new Promise(function (resolve, error) {
+        let pst = ins.ship_to(sm.ShipTo, stAID);
+        let psf = ins.ship_from(sm.ShipFrom, sfAID);
+
+        Promise.all([pst, psf]).then(addrIDs => {
+            let stID = addrIDs[0];
+            let sfID = addrIDs[1];
+            resolve([stID, sfID]);
+        });
+    })
+}
