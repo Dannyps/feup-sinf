5748030c463b62a17c124af91eb04461c1f6ff89
Daniel Silva <danny@dannyps.net>
2018-12-18 11:23:35 +0000

    update totalSales endpoint

--- server/routes/sales.js
+++ server/routes/sales.js
@@ -2,169 +2,175 @@ var express = require('express');
 var router = express.Router();
 
 /* GET sales by country listing. */
-router.get('/countrysales', function(req, res, next) {
-    var salesByCountry = [
-      {
-        country: "Germany",
-        sales: 200
-      },
-      {
-        country: "United States",
-        sales: 300
-      },
-      {
-        country: "Spain",
-        sales: 300
-      },
-      {
-        country: "Portugal",
-        sales: 400
-      },
-      {
-        country: "Russia",
-        sales: 150
-      },
-      {
-        country: "Japan",
-        sales: 100
-      },
+router.get('/countrysales', function (req, res, next) {
+  var salesByCountry = [{
+      country: "Germany",
+      sales: 200
+    },
+    {
+      country: "United States",
+      sales: 300
+    },
+    {
+      country: "Spain",
+      sales: 300
+    },
+    {
+      country: "Portugal",
+      sales: 400
+    },
+    {
+      country: "Russia",
+      sales: 150
+    },
+    {
+      country: "Japan",
+      sales: 100
+    },
 
-    ];
-    res.json(salesByCountry);
-  });
+  ];
+  res.json(salesByCountry);
+});
 
-router.get('/productsales', function(req, res, next) {
+router.get('/productsales', function (req, res, next) {
 
-  var productSales = [
-    {
-        'year': 2013,
-        'A': 120,
-        'B': 40,
-        'C': 50,
-        'D': 50,
-        'E': 60,
-        'F': 70,
+  var productSales = [{
+      'year': 2013,
+      'A': 120,
+      'B': 40,
+      'C': 50,
+      'D': 50,
+      'E': 60,
+      'F': 70,
     },
     {
-        'year': 2014,
-        'A': 110,
-        'B': 30,
-        'C': 80,
-        'D': 50,
-        'E': 60,
-        'F': 70,
+      'year': 2014,
+      'A': 110,
+      'B': 30,
+      'C': 80,
+      'D': 50,
+      'E': 60,
+      'F': 70,
     },
     {
-        'year': 2015,
-        'A': 70,
-        'B': 40,
-        'C': 50,
-        'D': 50,
-        'E': 60,
-        'F': 70,
+      'year': 2015,
+      'A': 70,
+      'B': 40,
+      'C': 50,
+      'D': 50,
+      'E': 60,
+      'F': 70,
     },
     {
-        'year': 2016,
-        'A': 110,
-        'B': 130,
-        'C': 90,
-        'D': 50,
-        'E': 60,
-        'F': 70,
+      'year': 2016,
+      'A': 110,
+      'B': 130,
+      'C': 90,
+      'D': 50,
+      'E': 60,
+      'F': 70,
     },
     {
-        'year': 2017,
-        'A': 110,
-        'B': 70,
-        'C': 60,
-        'D': 50,
-        'E': 60,
-        'F': 70,
+      'year': 2017,
+      'A': 110,
+      'B': 70,
+      'C': 60,
+      'D': 50,
+      'E': 60,
+      'F': 70,
     }
   ];
-    res.json(productSales);
+  res.json(productSales);
 });
 
 
-router.get('/totalsales', function(req, res, next) {
-    
-      var totalSales = [
-        {
-            'month': 'Jan',
-            'value': 11000
-        },
-        {
-            'month': 'Fev',
-            'value': 13000
-        },
-        {
-            'month': 'Mar',
-            'value': 10000
-        },
-    ];
+router.get('/totalsales', function (req, res, next) {
+
+  con.query("SELECT total_credit-total_debit as saldo FROM sales_invoices", function (err, result) {
+    var totalSales = [{
+      'month': 'Jan',
+      'value': result[0].saldo
+    }];
+
+
+    //   var totalSales = [
+    //     {
+    //         'month': 'Jan',
+    //         'value': 11000
+    //     },
+    //     {
+    //         'month': 'Fev',
+    //         'value': 13000
+    //     },
+    //     {
+    //         'month': 'Mar',
+    //         'value': 10000
+    //     },
+    // ];
     res.json(totalSales);
+  });
+
+
 });
 
 
 /* GET purchases volume. */
-router.get('/bestsellers', function(req, res, next) {
-  
-  var volume = [
-    {
-        'year': 2013,
-        'A': 120,
-        'B': 40,
-        'C': 50,
+router.get('/bestsellers', function (req, res, next) {
+
+  var volume = [{
+      'year': 2013,
+      'A': 120,
+      'B': 40,
+      'C': 50,
     },
     {
-        'year': 2014,
-        'A': 110,
-        'B': 30,
-        'C': 80,
+      'year': 2014,
+      'A': 110,
+      'B': 30,
+      'C': 80,
     },
     {
-        'year': 2015,
-        'A': 70,
-        'B': 40,
-        'C': 50,
+      'year': 2015,
+      'A': 70,
+      'B': 40,
+      'C': 50,
     },
     {
-        'year': 2016,
-        'A': 110,
-        'B': 130,
-        'C': 90,
+      'year': 2016,
+      'A': 110,
+      'B': 130,
+      'C': 90,
     },
     {
-        'year': 2017,
-        'A': 110,
-        'B': 70,
-        'C': 60,
+      'year': 2017,
+      'A': 110,
+      'B': 70,
+      'C': 60,
     }
   ];
-    res.json(volume);
+  res.json(volume);
 
-  });
+});
 
-  /* GET purchases volume. */
-router.get('/salesbycustomer', function(req, res, next) {
-    
-    var salesByCustomer = [
-      {
-        'customer': 'Customer A',
-        'value': 70000,
-      },
-      {
-        'customer': 'Customer B',
-        'value': 30000,
-      },
-      {
-        'customer': 'Customer C',
-        'value': 50000,
-      },
-    ];
-      res.json(volume);
-  
-    });
+/* GET purchases volume. */
+router.get('/salesbycustomer', function (req, res, next) {
+
+  var salesByCustomer = [{
+      'customer': 'Customer A',
+      'value': 70000,
+    },
+    {
+      'customer': 'Customer B',
+      'value': 30000,
+    },
+    {
+      'customer': 'Customer C',
+      'value': 50000,
+    },
+  ];
+  res.json(volume);
+
+});
 
 
 module.exports = router;
