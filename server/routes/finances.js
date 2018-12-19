b0c8da73c0e3aaafc4cb61bc01953b9a13dc4612
Daniel Silva <danny@dannyps.net>
2018-12-19 16:25:30 +0000

    Add cashbalance

--- server/routes/finances.js
+++ server/routes/finances.js
@@ -2,109 +2,94 @@ var express = require('express');
 var router = express.Router();
 
 /* GET equity values. */
-router.get('/equity', function(req, res, next) {
-  
-  var equity = [
-    {
-        'year': 2013,
-        'value': 50000
-    },
-    {
-        'year': 2014,
-        'value': 60000
-    },
-    {
-        'year': 2015,
-        'value': 30000
-    },
-    {
-        'year': 2016,
-        'value': 25000
-    },
-    {
-        'year': 2017,
-        'value': 60000
-    }
-  ];
-  res.json(equity);
+router.get('/equity', function (req, res, next) {
+
+    var equity = [{
+            'year': 2013,
+            'value': 50000
+        },
+        {
+            'year': 2014,
+            'value': 60000
+        },
+        {
+            'year': 2015,
+            'value': 30000
+        },
+        {
+            'year': 2016,
+            'value': 25000
+        },
+        {
+            'year': 2017,
+            'value': 60000
+        }
+    ];
+    res.json(equity);
 });
 
 
 /* GET liability values. */
-router.get('/liability', function(req, res, next) {
-  
-  var liability = [
-    {
-        'year': 2013,
-        'value': 25000
-    },
-    {
-        'year': 2014,
-        'value': 22000
-    },
-    {
-        'year': 2015,
-        'value': 28000
-    },
-    {
-        'year': 2016,
-        'value': 34000
-    },
-    {
-        'year': 2017,
-        'value': 17000
-    }
-  ];
-  res.json(liability);
+router.get('/liability', function (req, res, next) {
+
+    var liability = [{
+            'year': 2013,
+            'value': 25000
+        },
+        {
+            'year': 2014,
+            'value': 22000
+        },
+        {
+            'year': 2015,
+            'value': 28000
+        },
+        {
+            'year': 2016,
+            'value': 34000
+        },
+        {
+            'year': 2017,
+            'value': 17000
+        }
+    ];
+    res.json(liability);
 });
 
 /* GET asset values. */
-router.get('/assets', function(req, res, next) {
-  
-  var assets = [
-    {
-        'year': 2013,
-        'value': 22000
-    },
-    {
-        'year': 2014,
-        'value': 33000
-    },
-    {
-        'year': 2015,
-        'value': 18000
-    },
-    {
-        'year': 2016,
-        'value': 11000
-    },
-    {
-        'year': 2017,
-        'value': 12000
-    }
-  ];
-  res.json(assets);
+router.get('/assets', function (req, res, next) {
+
+    var assets = [{
+            'year': 2013,
+            'value': 22000
+        },
+        {
+            'year': 2014,
+            'value': 33000
+        },
+        {
+            'year': 2015,
+            'value': 18000
+        },
+        {
+            'year': 2016,
+            'value': 11000
+        },
+        {
+            'year': 2017,
+            'value': 12000
+        }
+    ];
+    res.json(assets);
 });
 
 /* GET cash values (cash balance). */
-router.get('/cashbalance', function(req, res, next) {
-    
-    var cashBalance = [
-      {
-          'month': 'Jan',
-          'value': 11000
-      },
-      {
-          'month': 'Fev',
-          'value': 13000
-      },
-      {
-          'month': 'Mar',
-          'value': 10000
-      },
-    ];
-    res.json(cashBalance);
-  });
+router.get('/cashbalance', function (req, res, next) {
+
+    con.query("select sum(closing_debit_balance)-sum(closing_credit_balance) as balance from account where id like \"1%\" and LENGTH(id) = 2", function (err, result) {
+        res.json(result[0].balance);
+    });
+});
 
 
 module.exports = router;
