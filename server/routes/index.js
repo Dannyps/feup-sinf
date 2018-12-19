38ba9b84a2c37673ee9ffccb203df85a2b4f9633
Daniel Silva <danny@dannyps.net>
2018-12-19 16:42:18 +0000

    Add company name and fiscal year to the page

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/info', function(req, res, next) {
+  con.query("SELECT * FROM header", function (err, result) {
+    res.json(result[0]);
+  });
+});
+
 module.exports = router;
